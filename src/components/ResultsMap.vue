f5a8fc2bfd23a7a9710f9f016853620cf419407c
Jiale Cao <Jiale.Cao@gov.mb.ca>
2025-02-19 09:25:03 -0600

    Fix realationship list name issue

--- src/components/ResultsMap.vue
+++ src/components/ResultsMap.vue
@@ -1893,7 +1893,7 @@ defineExpose({
     <!-- <h4>Bubble</h4> -->
     <el-form @submit.prevent="updateBubbleText" style="margin: 0 0 10px 0">
       <el-form-item label="">
-        <el-input v-model="newText" type="textarea" />
+        <el-input v-model="newText" autosize type="textarea" />
       </el-form-item>
       <el-form-item class="margin-top-less">
         <el-button type="primary" style="width: 47%" @click="updateBubbleText">Update</el-button>
@@ -1941,7 +1941,7 @@ defineExpose({
         >
           <span class="text-ellipsis">
             <el-icon style="vertical-align: middle; margin-right: 2px"><Link /></el-icon>
-            {{ props.data.bubbles.find((b) => b.id === relationship.target)?.text || '' }}</span
+            {{props.data.bubbles.find((b) => selectedBubble?.id === relationship.target?  b.id === relationship.source :  b.id === relationship.target)?.text }}</span
           >
 
           <el-row :gutter="10">
@@ -1982,7 +1982,7 @@ defineExpose({
     <el-divider> Bubble </el-divider>
     <el-form @submit.prevent="createBubble">
       <el-form-item label="">
-        <el-input v-model="newBubbleText" type="textarea" />
+        <el-input v-model="newBubbleText" autosize type="textarea" />
       </el-form-item>
       <el-form-item>
         <el-button
